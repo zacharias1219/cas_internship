f2b07caf5de3de2fe9fdc09b4049a10b4b3cf8cd
Richard Abishai <rickoshade1891@gmail.com>
2024-07-16 14:24:00 +0530

    s

--- pages/Paths1.py
+++ pages/Paths1.py
@@ -324,7 +324,7 @@ def picture_quiz_template(data, question_number):
     questions = data['questions']
     question = questions[current_question_index]
     audio_response_path = text_to_speech(question)
-    st.audio(audio_response_path, format="audio/mp3", start_time=0)
+    autoplay_audio(audio_response_path)
 
     if current_question_index > 0:
         st.markdown("Well Done, Next Question")
